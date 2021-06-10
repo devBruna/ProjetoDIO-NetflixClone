41382ccb70dd1549bf362acca4b85b129b6c224c
devBruna <85190975+devBruna@users.noreply.github.com>
2021-06-10 12:40:58 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        500:{
+            items:3
+        },
+        700:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
